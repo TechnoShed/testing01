53b3170533f4ddba70b4e07bcc028f97e9c0b17d
technoshed <info@technoshed.uk>
2023-02-20 23:47:02 +0000

    modified:   main.py     Basic layout setup 	new file:   readme.md

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return "<h1>\nHello, World!</h1>"
 
 print(txtline+"\nhi\n"+txtline)
 
---
+++ readme.md
@@ -0,0 +1,1 @@
+Web Application to interacte with an mysql back end database or fleet vehicles
